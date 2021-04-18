8bb6642985e870d54a90f1e3a3a8ce7338954ce2
amardins <andi.syahra@gmail.com>
2021-04-18 21:25:42 +0700

    crawler

--- web_scraping/web_scraping/writer.py
+++ web_scraping/web_scraping/writer.py
@@ -11,3 +11,10 @@ def write_to_postgres(df: pd.DataFrame, db_name: str, table_name: str, connectio
 
     logging.info(f"Writing dataframe to database: '{db_name}', table: '{table_name}' ...")
     df.to_sql(table_name, con=engine, if_exists="replace", index=False)
+
+
+def append_to_postgres(df: pd.DataFrame, db_name: str, table_name: str, connection_string: str) -> None:
+    engine = create_engine(connection_string)
+
+    logging.info(f"Writing dataframe to database: '{db_name}', table: '{table_name}' ...")
+    df.to_sql(table_name, con=engine, if_exists="append", index=False)
